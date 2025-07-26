2dc27f59288ffeeb2000d6e11987254503b70085
ikramzargar <ikramxargar@gmail.com>
2025-07-26 16:34:29 +0530

    setup login signup without otp

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,44 +1,31 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class AppUser {
-  const AppUser({
-    required this.uid,
-    required this.firstName,
-    required this.lastName,
-    required this.phone,
-    required this.email,
-    required this.isPaid,
-    required this.isPhoneVerified,
-  });
-
-  final String uid;
+class UserModel {
   final String firstName;
   final String lastName;
-  final String phone;
   final String email;
-  final bool isPaid;
-  final bool isPhoneVerified;
+  final String phone;
 
-  factory AppUser.fromFirestore(DocumentSnapshot<Map<String, dynamic>> doc) {
-    final data = doc.data() ?? {};
-    return AppUser(
-      uid: doc.id,
-      firstName: data['firstName'] ?? '',
-      lastName: data['lastName'] ?? '',
-      phone: data['phone'] ?? '',
-      email: data['email'] ?? '',
-      isPaid: data['isPaid'] ?? false,
-      isPhoneVerified: data['isPhoneVerified'] ?? false,
-    );
+  UserModel({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.phone,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'first_name': firstName,
+      'last_name': lastName,
+      'email': email,
+      'phone': phone,
+    };
   }
 
-  Map<String, dynamic> toMap() => {
-    'firstName': firstName,
-    'lastName': lastName,
-    'phone': phone,
-    'email': email,
-    'isPaid': isPaid,
-    'isPhoneVerified': isPhoneVerified,
-    'updatedAt': FieldValue.serverTimestamp(),
-  };
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      firstName: map['first_name'] ?? '',
+      lastName: map['last_name'] ?? '',
+      email: map['email'] ?? '',
+      phone: map['phone'] ?? '',
+    );
+  }
 }
